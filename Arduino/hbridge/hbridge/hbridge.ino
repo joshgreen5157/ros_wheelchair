c59f8536c0f5b07d4b7ec4dd9ce2655cd0a06ae2
joshgreen5157 <59094375+joshgreen5157@users.noreply.github.com>
2022-02-26 09:30:43 -0500

    state built

--- Arduino/hbridge/hbridge/hbridge.ino
+++ Arduino/hbridge/hbridge/hbridge.ino
@@ -1,4 +1,5 @@
 #include<Arduino.h>
+#include<String.h>
 
 //Pins
 const int ENA = 8; //Define enable pin for Megamoto 1
@@ -7,19 +8,31 @@ const int LMR = 5; //Define pin for Left Motor Reverse
 const int RMF = 9; //Define pin for Right Motor Forward
 const int RMR = 10; //Define pin for Right Motor Reverse
 const int JOYX = 0; const int JOYY = 1;
+//const int BRAKED = 13; //brake Input
 
 //Range variables
-const int joyRange = 500;
+int xCal, yCal;
 const int turnReduction = 2;
 const int motorRange = 100;
-const int joyPlay = 10;
+const int joyPlay = 30;
+const int calibrationlevel = 10;
 
 //Operational Functions
 void brake(); void coast(); void motorLeft(int speed=0); void motorRight(int speed=0);
-void writeMotors(int left, int right); void serialRead(); void readJoystick();
+void writeMotors(int left, int right); void serialRead(); 
+void readJoystick(); void calibrateJoystick();
 
 //timing variables
-int cmdDelayMillis = 0; int readTimer = 500;
+unsigned long timer; unsigned long timer2; unsigned long cmdTimer = 0; unsigned long joyTimer = 0;
+const int cmdDelay1 = 250;  const int cmdDelay2 = 25;
+const int brakeDelay = 100; 
+const int autoDelay = 500; const int joyDelay = 500; 
+
+//state variables
+bool stateChanged = true;
+int state = 0; //0: brake, 1: manual, 2: joystick, 3: autonomous;
+bool joyReadyBool = false; bool autonomousModeBool = false;
+bool joyModeBool = false;
 
 void setup() {
   Serial.begin(9600); // start Serial communication
@@ -30,107 +43,159 @@ void setup() {
   pinMode(RMR, OUTPUT); 
   pinMode(JOYX, INPUT);
   pinMode(JOYY, INPUT);
-  brake();
+//  pinMode(BRAKED, INPUT);
+  calibrateJoystick();
 }
+
+void calibrateJoystick() {
+  unsigned int xTot= 0; unsigned int yTot = 0;
+  for (int i = 0; i < calibrationlevel; i++) {
+    xTot = xTot + analogRead(JOYX);
+    yTot = yTot + analogRead(JOYY);
+  }
+  unsigned int xAvg = xTot / calibrationlevel;
+  unsigned int yAvg = yTot / calibrationlevel;
+  //magic numbers, need to make tied to something
+  if (xAvg < 475 || xAvg > 575) {
+    xAvg = 525;
+  }
+  if (xAvg < 475 || yAvg > 575) {
+    yAvg = 525;
+  }
+  xCal = xAvg;
+  yCal = yAvg;
+  joyReadyBool = true;
+  joyModeBool = true;
+  }
 
 //wheels braking
 void brake(){
-  digitalWrite(ENA, HIGH);
-  motorLeft();
-  motorRight();
-}
-
-void coast() {
   digitalWrite(ENA, LOW);
-  //bring motors to 0
   motorLeft();
   motorRight();
 }
 
 //should be able to turn these into one function
-void motorLeft(int speed){
+void motorLeft(int speed=0){
   if (speed == 0) {
       analogWrite(LMR, 0);
       analogWrite(LMF, 0);
     }
     else if (speed < 0 ) {
+      Serial.println((String)"Writing LEFT motor REVERSE: " + speed);
       analogWrite(LMR, -speed);
       analogWrite(LMF, 0);
     }
-    else {
+    else if (speed > 0) {
       analogWrite(LMR, 0);
       analogWrite(LMF, speed);
     }
   };
 
-void motorRight(int speed){
+void motorRight(int speed=0){
   if (speed == 0) {
     analogWrite(RMR, 0);
     analogWrite(RMF, 0);
   }
     else if (speed < 0 ) {
+      Serial.println((String)"Writing RIGHT motor REVERSE: " + speed);
       analogWrite(RMR, -speed);
       analogWrite(RMF, 0);
     }
-    else {
+    else if (speed > 0) {
       analogWrite(RMR, 0);
       analogWrite(RMF, speed);
     }
 };
 
 void writeMotors(int left, int right){
-  motorLeft(left);
-  motorRight(right);
+  digitalWrite(ENA, HIGH);
+  motorLeft(left/8);
+  motorRight(right/8);
 }
 
 void readJoystick() {
-  // Serial.print("JOYX: ");
-  // Serial.println(analogRead(JOYX));
-  // Serial.print("JOYY: ");
-  // Serial.println(analogRead(JOYY));
-  //values read in at 0-1000, converting to -500 to 500
-  Serial.println("Reading Joystick");
-  int xValue; int yValue;
-  xValue = analogRead(JOYX)-500;
-  yValue = analogRead(JOYY)-500;
-  if (yValue / joyPlay == 0) {
-    yValue =  0;
-  }
-  if (xValue / joyPlay == 0) {
-    xValue = 0;
-  }
-  if (xValue != 0) 
-  { xValue = xValue / turnReduction;  }
+  if (timer - joyTimer > joyDelay) {
+    joyTimer = millis();;
+    int xValue; int yValue;
+    xValue = analogRead(JOYX)-xCal;
+    yValue = analogRead(JOYY)-yCal;
+    if (yValue / joyPlay == 0) {
+      yValue =  0;
+    }
+    if (xValue / joyPlay == 0) {
+      xValue = 0;
+    }
+    if (xValue != 0) 
+    { xValue = xValue / turnReduction;  }
+     Serial.println((String)"XXXXXX: " + xValue + " || YYYYYY " + yValue);
+    int leftPower = yValue + xValue; int rightPower = yValue - xValue;
+    Serial.println((String)"Left motor: " + leftPower + " || Right motor: " + rightPower);
+    writeMotors(leftPower, rightPower);
+}}
+
+void autonomousMode() {
+  Serial.read();
 }
 
-void serialRead() { 
-
-  int leftInt = yValue - xValue;
-  int rightInt = yValue + xValue;
-  leftInt = map(leftInt, -joyRange, joyRange, -motorRange, motorRange);
-  rightInt = map(rightInt, -joyRange, joyRange, -motorRange, motorRange);
-  writeMotors(leftInt, rightInt);
-}
 
 void checkState() {
-  //copy current state to temporary variable
-  //check pins for status
-  //update state value
-  //if state != state then do nothing
-  //else go to run()
-
+  int tempState;
+  serialRead();
+  bool BRAKED=false;
+  if (BRAKED== true) {
+    tempState = 0;
+  }
+  else if (joyReadyBool == true && joyModeBool == true) {
+    tempState = 2;
+  }
+  else if (autonomousModeBool == true && joyModeBool == false) {
+    tempState = 3;
+  }
+  else {
+    tempState = 1;
+  }
+  if (tempState != state) {
+    stateChanged = true;
+    state = tempState;
+    Serial.println((String)"***State Changed***    " + state + state + state + state );
+    
+  }
 }
 
-void run() {
-  //switch case with state variable
-
+void serialRead() {
+  if (Serial.available() > 0) {
+    autonomousModeBool = true; 
+  }
 }
-
 
 void loop() {
-
-  //if some signal:
-  //run()
-  //else delay for milli time
-
+  timer = millis();
+  if (timer - timer2 > cmdDelay2) {
+    checkState();
+    timer2 = millis();
+  }
+  if (timer - cmdDelay1 > cmdTimer) {
+    switch (state) {
+      case 1: //manual state
+      Serial.println("Manual");
+      brake();
+      break;
+      case 2: //joystick state
+      readJoystick();
+      break;
+      case 3: //autonomous state
+      Serial.println("Autonomous");
+      break;
+      default:
+      //brake state
+      brake();
+      Serial.println("Braked");
+      break;
+    }
+    if (stateChanged) {
+      stateChanged = false;
+    }
+    cmdTimer = millis();
+  }
 } // end of code
